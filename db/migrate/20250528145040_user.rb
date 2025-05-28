41ca99484980a012557dc589bbf083569c3e4ac7
Rin0cc <nha587260@gmail.com>
2025-05-29 00:05:43 +0900

    bin/render-build.sh

---
+++ db/migrate/20250528145040_user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, null: false, default: ""
+      t.timestamps 
+    end
+  end
+end
