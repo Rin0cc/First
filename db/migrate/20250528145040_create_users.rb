6932c6a7816cc0bba34df57ce3ba97afde19ac30
Rin0cc <nha587260@gmail.com>
2025-06-05 20:50:07 +0900

    fix:robocup修正

--- db/migrate/20250528145040_create_users.rb
+++ db/migrate/20250528145040_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.2]
     create_table :users do |t|
       t.string :name
       t.string :email, null: false, default: ""
-      t.timestamps 
+      t.timestamps
     end
   end
 end
