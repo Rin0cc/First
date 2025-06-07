cc79cd76cd1189b08afb29f4cf865cb16f68df0f
Rin0cc <nha587260@gmail.com>
2025-06-07 21:17:19 +0900

    add:ストロングパラーメーター追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)  # user_paramsで安全に受け取る
+      redirect_to @user, notice: "更新しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+  end
 end
