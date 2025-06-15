e367c4089d13af9b840e6c1ca4b07ef1a72adf9e
Rin0cc <nha587260@gmail.com>
2025-06-15 21:39:52 +0900

    fix:デプロイエラー訂正

--- app/models/user_flower.rb
+++ app/models/user_flower.rb
@@ -4,19 +4,20 @@ class UserFlower < ApplicationRecord
   has_many :records, dependent: :destroy
 
   enum status: {
-  seed: "種",
-  sprout: "芽",
-  bud: "水やり",
-  full_bloom: "満開"
+    seed: "種",
+    sprout: "芽",
+    bud: "水やり",
+    full_bloom: "満開"
   }
 
-validate :only_one_growing_flower_per_user, on: :create
+  validate :only_one_growing_flower_per_user, on: :create
 
-private
+  private
 
-   # 同時に育てている花があればNG（満開以外が育成中とみなす）
-   def only_one_growing_flower_per_user
-    if user.user_flowers.where.not(status: "full_bloom").exists?
+  def only_one_growing_flower_per_user
+    return if user.nil?
+
+    if user.user_flowers.where.not(status: :full_bloom).exists?
       errors.add(:base, "同時に育てられる花は1つだけです。")
     end
   end
