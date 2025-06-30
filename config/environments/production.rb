76763ff2f4c45f3975f40699e212a4b1d36cb83e
Rin0cc <nha587260@gmail.com>
2025-06-30 21:59:58 +0900

    add:bloomingrecord.comサブドメイン

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << 'bloomingrecord.com'
+  config.hosts << 'www.bloomingrecord.com'
 end
