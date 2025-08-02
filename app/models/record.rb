3485e2aa67abe0a70b3f66a4c33f98f7c35af135
Rin0cc <nha587260@gmail.com>
2025-08-02 21:15:28 +0900

    fix:detabaseのflwer再度追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,10 +1,13 @@
+# app/models/record.rb
+
 class Record < ApplicationRecord
   belongs_to :user
-  belongs_to :user_flower, optional: true # optional: true を追加した理由を後述
+  belongs_to :user_flower, optional: true
 
-  # 時間記録の場合とToDoの場合でバリデーションを分ける
-  # time が存在しない（nilか0）場合は task_name が必須
-  # time が存在する（0より大きい）場合は task_name は必須ではない
+  scope :completed, -> { where(completed: true) }
+
+  scope :incomplete, -> { where(completed: false) }
+
   validates :task_name, presence: true, unless: :time_record?
   validates :time, presence: true, numericality: { greater_than_or_equal_to: 0 }, unless: :task_only_record?
 
@@ -17,10 +20,4 @@ class Record < ApplicationRecord
   def task_only_record?
     task_name.present? && (time.nil? || time == 0)
   end
-
-  # モデルが作成される前に user_flower が存在するか確認する
-  # これはRecordsController#set_user_flowerでUserFlowerが必ず設定されるため
-  # モデルレベルで必須とする必要はないが、もし必要ならpresence: trueをtime_record? と task_only_record? のどちらかで制御する
-  # current_user.user_flowers.build(record_params.merge(user: current_user)) で構築されるため、
-  # Recordモデルでuser_flower_idの存在チェックは不要（またはconditional）
 end
