05ecd35f679f20ccb08182f1fa42a031dadd3f5b
Rin0cc <nha587260@gmail.com>
2025-08-05 21:20:28 +0900

    fix:deviseのrequire訂正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-require 'omniauth-rails_csrf_protection'
+require 'omniauth/rails_csrf_protection' 
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
