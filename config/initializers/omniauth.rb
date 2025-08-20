e474aa60869433a64f05b8675939c0f1138eb7f3
Rin0cc <nha587260@gmail.com>
2025-08-20 20:24:17 +0900

    googleログイン13

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-google_credentials = Rails.application.credentials.google
-
-OmniAuth.config.allowed_request_methods = %i[get post]
-OmniAuth.config.silence_get_warning = true
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, google_credentials[:client_id], google_credentials[:client_secret]
-end
