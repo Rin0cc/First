36b4d5bf8eafb6ee3dc8154d04cff63464f48b73
Rin0cc <nha587260@gmail.com>
2025-09-05 20:14:14 +0900

    fix:google認証14

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -619,9 +619,9 @@ Devise.setup do |config|
   config.omniauth :google_oauth2,
     ENV["GOOGLE_CLIENT_ID"],
     ENV["GOOGLE_CLIENT_SECRET"],
-  scope: "userinfo.email",
-  access_type: "offline",
-  prompt: "consent"
+    scope: "userinfo.email",
+    access_type: "offline",
+    prompt: "consent"
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
