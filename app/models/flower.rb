032235b7dca0665947730cdd61249b8396765dfa
Rin0cc <nha587260@gmail.com>
2025-10-02 23:32:37 +0900

    fix;図鑑訂正

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -2,4 +2,13 @@ class Flower < ApplicationRecord
   has_many :user_flowers, dependent: :destroy
 
   validates :name, presence: true
+
+  # 画像パスを安全に扱うメソッド
+  def bloom_image_path
+    path = self[:bloom_image_path] # DBに保存されている値を直接参照
+    return "placeholder_flower.png" if path.blank?
+
+    # "image/" がついていたら除去して返す
+    path.sub(/^image\//, "")
+  end
 end
