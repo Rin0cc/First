7d0ebc2f64b629812183ab9760f2dac395b180e3
Rin0cc <nha587260@gmail.com>
2025-11-01 21:54:27 +0900

    fix:エラー修正rubocup

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   }
   get "/sign_up", to: redirect("/users/sign_up")
   devise_scope :user do
-  post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
 end
 
   root "top#index"
